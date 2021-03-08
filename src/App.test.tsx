6bf8e7ffd559e6dd52dc8f6c71e4d1a95036147c
Pyry Pajunpää <pyry@identio.fi>
2021-03-08 16:10:21 +0200

    build folder

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Such high tech/i);
   expect(linkElement).toBeInTheDocument();
 });
