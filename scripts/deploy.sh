2c540a4c9335ad29740bd93d4d88d4adc1dcc966
Pyry Pajunpää <pyry@identio.fi>
2021-03-09 22:30:42 +0200

    stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,21 +11,6 @@ aws s3 sync ../build s3://$S3_BUCKET_NAME/ \
   --exclude service-worker.js \
   --exclude index.html
 
-echo "Uploading service-worker.js"
-aws s3 cp ../build/service-worker.js s3://$S3_BUCKET_NAME/service-worker.js \
-  --metadata-directive REPLACE \
-  --cache-control max-age=0,no-cache,no-store,must-revalidate \
-  --content-type application/javascript \
-  --acl public-read
-
-# Upload index.html
-echo "Uploading index.html"
-aws s3 cp ../build/index.html s3://$S3_BUCKET_NAME/index.html \
-  --metadata-directive REPLACE \
-  --cache-control max-age=0,no-cache,no-store,must-revalidate \
-  --content-type text/html \
-  --acl public-read
-
 # # Purge the cloudfront cache
 # echo "Purging the cache for CloudFront"
 # aws cloudfront create-invalidation \
