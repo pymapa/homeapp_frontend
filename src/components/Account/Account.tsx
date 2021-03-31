2a6a730b750730e465366967a648b014757ecef2
Pyry Pajunpää <pyry@identio.fi>
2021-03-31 11:21:03 +0300

    actions

---
+++ src/components/Account/Account.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {}
+
+function Account(props: Props) {
+  const {} = props
+
+  return (
+    <div className="account">
+      <h1>Hello account</h1>
+    </div>
+  )
+}
+
+export default Account
